993443393e9678ca200eceffe87f45699f5784a0
freshlab-web <almir@freshlab.com.br>
2025-06-06 20:18:22 +0000

    change logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between">
           <a href="#" className="z-50">
             <img
-              src="/lovable-uploads/fc743b2e-b47e-455c-a025-daf0e32c8796.png"
+              src="https://cdn.jsdelivr.net/gh/freshlab-web/talkaio-lp-medico@main/dist/lovable-uploads/fc743b2e-b47e-455c-a025-daf0e32c8796.png"
               alt="Talkaio Logo"
               className="h-10"
             />
