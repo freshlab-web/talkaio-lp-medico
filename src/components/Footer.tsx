ec244a8b5339956b8dee6b3b5fce5afdd474d782
freshlab-web <almir@freshlab.com.br>
2025-06-13 19:55:59 +0000

    troca do numero de telefone e end. email

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -73,8 +73,8 @@ export default function Footer() {
               </li>
               <li className="flex items-start">
                 <Phone size={20} className="mr-3 mt-1 text-talkaio-blue" />
-                <a href="tel:+5511999999999" className="text-gray-300 hover:text-talkaio-blue transition-colors">
-                  (11) 99999-9999
+                <a href="tel:+5511918257335" className="text-gray-300 hover:text-talkaio-blue transition-colors">
+                  (11) 91825-7335
                 </a>
               </li>
               <li className="flex items-start">
